2379d5e7ab4568ae5535e623568233fa548aa62b
prabinshrestha323 <prabinshrestha323@gmail.com>
2020-01-24 18:21:14 +0545

    add the commit

---
+++ prototype.js
@@ -0,0 +1,15 @@
+function student() {
+  this.name = "Prabin";
+  this.age = "24";
+  this.email = "info@prabin.com";
+}
+
+student.prototype = {
+  address: "ktm",
+  getName: function() {
+    return this.name;
+  }
+};
+
+var std = new student();
+console.log(std);
